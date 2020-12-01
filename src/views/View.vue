2f7b9b34debe3ee9d5ad6a22eb71e6e683b75443
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2020-12-02 00:26:31 +0800

    implement single view

--- src/views/View.vue
+++ src/views/View.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="view">
-    view
-  </div>
+<div class="view">
+  <h2 v-html="currentElement"></h2>
+</div>
 </template>
-
 <script>
 export default {
-name: "View"
+  name: 'ViewSingle',
+  data () {
+    return {
+      currentElement: ''
+    }
+  },
+  mounted () {
+    // console.log(this.$route.params.tag[0])
+    this.currentElement = JSON.parse(localStorage.getItem('taskList'))[
+      this.$route.params.tag[0] - 1
+    ]
+  }
 }
 </script>
-
-<style>
-
+<style scoped>
+.view{
+  margin:  10rem auto;
+}
 </style>
