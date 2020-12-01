2ee799c296ef1552c1dbb6821dc22c9efb4cffda
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2020-12-02 00:22:40 +0800

    install syncfusion and styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { RichTextEditorPlugin } from '@syncfusion/ej2-vue-richtexteditor'
+
+Vue.use(RichTextEditorPlugin)
 
 Vue.config.productionTip = false
 
